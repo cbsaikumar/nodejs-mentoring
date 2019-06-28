9ed3e92d682c6e10ebacb8fa7bf2893ac9e7f0db
bhargava.chittapudi <bhargava.chittapudi@thomsonreuters.com>
2019-06-28 15:14:36 +0530

    build folder

---
+++ build/models/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Product = require('./Product');
+
+Object.defineProperty(exports, 'Product', {
+  enumerable: true,
+  get: function get() {
+    return _Product.Product;
+  }
+});
+
+var _User = require('./User');
+
+Object.defineProperty(exports, 'User', {
+  enumerable: true,
+  get: function get() {
+    return _User.User;
+  }
+});
